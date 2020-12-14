fc9c27227779ac28139764af7bfb25d3f1004dc2
Tumucin <baltumucin34@gmail.com>
2020-12-14 21:15:23 +0300

    a

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,18 +4,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import { TextField } from '@material-ui/core';
 import './index.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
-}));
 
 function BasicTextFields() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
   const classes = useStyles();
-
+  
   return (
     <form className={classes.root} noValidate autoComplete="off">
       <TextField id="standard-basic" label="Standard" />
